faacca430aae6cc393c74e0bf59bbb78651ed3ea
Keith Dimeni <keithdimeni@gmail.com>
2022-10-07 23:25:50 -0300

    Formas corretas de escrita de variaves

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,18 @@
 public class MyClass {
     public static void main(String [] args) {
         System.out.println("Hello World em Java!");
+
+        /**
+         * Formas corretas de escrita de variavel
+         */
+
+        final String BRASIL = "Brasil"; //constante
+
+        String nome = "Keith";
+        int numero1 = 1;
+        double _numeroDecimal = 12.4;
+        String $comSimboloDolar = "$$$";
+
+
     }
 }
